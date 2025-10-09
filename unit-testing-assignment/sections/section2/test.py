637caebc5e8d9e86a7ff4cd620e0e45245fe46ce
moniesco22 <122181156+moniesco22@users.noreply.github.com>
2025-10-08 22:36:36 -0400

    section 1-3 commits

--- unit-testing-assignment/sections/section2/test.py
+++ unit-testing-assignment/sections/section2/test.py
@@ -5,7 +5,7 @@ class TestCalculations(unittest.TestCase):
 
     def test_sum(self):
         calculation = Calculations(8, 2)
-        self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+        self.assertEqual(calculation.get_sum(), 11, 'The sum is wrong.')
 
 if __name__ == '__main__':
     unittest.main()
